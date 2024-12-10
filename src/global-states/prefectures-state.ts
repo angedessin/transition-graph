7b132464c4e7660ceffaf9b4e7f0211d1bf4f539
Kentaro Otsuka <rabizou.dev@gmail.com>
2024-12-10 16:17:08 +0900

    add : comment

--- src/global-states/prefectures-state.ts
+++ src/global-states/prefectures-state.ts
@@ -2,22 +2,21 @@ import { atom } from 'jotai';
 import { useAtomValue, useSetAtom } from 'jotai/index';
 import { useCallback } from 'react';
 
-export type PrefecturesState = {
-  checkedIds: string[];
-};
-
 export type UsePrefecturesMutators = {
   setPrefecturesAtomIds: (ids: string[]) => void;
 };
 
 // atom
 const prefecturesAtomIds = atom<string[]>([]);
-
 prefecturesAtomIds.debugLabel = 'prefecturesAtomIds';
 
+// mutators
 export const usePrefecturesMutators = (): UsePrefecturesMutators => {
   const setStateIds = useSetAtom(prefecturesAtomIds);
 
+  /**
+   * 選択された都道府県のIDを更新
+   */
   const setPrefecturesAtomIds = useCallback(
     (ids: string[]) => {
       setStateIds(ids);
@@ -28,6 +27,7 @@ export const usePrefecturesMutators = (): UsePrefecturesMutators => {
   return { setPrefecturesAtomIds };
 };
 
+// value
 export const usePrefecturesIdsState = (): string[] => {
   return useAtomValue(prefecturesAtomIds);
 };
