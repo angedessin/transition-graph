79aa72df34b04455a8526d521d1403134c5073de
Kentaro Otsuka <rabizou.dev@gmail.com>
2024-12-11 10:24:56 +0900

    update : meta

--- src/config/meta.ts
+++ src/config/meta.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = 'タイトル';
-export const SITE_DESCRIPTION = 'ディスクリプション';
+export const SITE_TITLE = '都道府県別の総人口推移グラフ';
+export const SITE_DESCRIPTION = '都道府県別の人口推移グラフを表示させる。';
 export const SITE_URL = 'https://nextjs.org/';
